6972f19102c2bd698b0335e97948cb94a94e71c3
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-03-16 21:18:13 +0300

    edit greeting resource

--- src/main/java/com/edward/fullsatck/GreetingResource.java
+++ src/main/java/com/edward/fullsatck/GreetingResource.java
@@ -1,8 +1,10 @@
 package com.edward.fullsatck;
 
+import jakarta.transaction.Transactional;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
 import org.jboss.resteasy.reactive.RestQuery;
 
@@ -11,7 +13,11 @@ public class GreetingResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello(@RestQuery String name) {
+    @Transactional
+    public String hello(@QueryParam("name") String name) {
+        Greeting greeting = new Greeting();
+        greeting.name = name;
+        greeting.persist();
         return "Hi " + name;
     }
 }
