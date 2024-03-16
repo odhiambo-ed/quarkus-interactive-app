6bdca4d2928cdee0f86c27b243684ea1a6774ba1
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-03-16 21:04:41 +0300

    add greeting entity

---
+++ src/test/java/com/edward/fullsatck/Greeting.java
@@ -0,0 +1,10 @@
+package com.edward.fullsatck;
+
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import jakarta.persistence.Entity;
+
+@Entity
+public class Greeting extends PanacheEntity {
+    public String name;
+}
